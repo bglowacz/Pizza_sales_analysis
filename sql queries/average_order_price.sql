dcfc6846f38181194d2df8bc4d408e6f5526b544
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-04-04 09:13:17 +0200

    Update average_order_price.sql

--- sql queries/average_order_price.sql
+++ sql queries/average_order_price.sql
@@ -1,2 +1,8 @@
-INSERT INTO avg_order_price (average_order_price) VALUES
-	 (16.82);
+with 
+	avg_order_price as (select p.pizza_id, pt.category, od.order_id,  p.price*od.quantity as order_price  from pizzas p 
+	join order_details od on p.pizza_id = od.pizza_id
+	join orders o on o.order_id = od.order_id 
+	join pizza_types pt on pt.pizza_type_id = p.pizza_type_id
+	group by 1,2,3,4)
+
+select round(avg(order_price),2) as average_order_price from avg_order_price
