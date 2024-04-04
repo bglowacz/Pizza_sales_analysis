511804d3aa220da21ca941832a208234fd65b60b
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-04-04 09:47:38 +0200

    Update total_revenue_by_month.sql

--- sql queries/total_revenue_by_month.sql
+++ sql queries/total_revenue_by_month.sql
@@ -1,14 +1,7 @@
-INSERT INTO month_and_revenue (`month`,total_revenue) VALUES
-	 ('January',3034.75),
-	 ('February',2774.7),
-	 ('March',2887.75),
-	 ('April',2940.2500000000005),
-	 ('May',3068.5),
-	 ('June',2941.9500000000003),
-	 ('July',2882.8),
-	 ('August',2869.95),
-	 ('September',2973.75),
-	 ('October',2893.0);
-INSERT INTO month_and_revenue (`month`,total_revenue) VALUES
-	 ('November',2728.7),
-	 ('December',2880.25);
+with 
+	order_prices as (select date_format(o.date, "%M") as month, p.pizza_id, od.quantity, p.price, p.price*od.quantity as order_total_price  from pizzas p 
+	join order_details od on p.pizza_id = od.pizza_id
+	join orders o on o.order_id = od.order_id 
+	group by 1,2,3,4),
+	month_and_revenue as (select month, sum(order_total_price) as total_revenue from order_prices group by 1)
+select month, total_revenue from month_and_revenue
