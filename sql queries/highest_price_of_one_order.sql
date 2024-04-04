18531005cb1dbe9fed87c0139013654763bcaaa5
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-04-04 09:37:54 +0200

    Update highest_price_of_one_order.sql

--- sql queries/highest_price_of_one_order.sql
+++ sql queries/highest_price_of_one_order.sql
@@ -1,2 +1,7 @@
-INSERT INTO avg_order_price (highest_price_of_one_order) VALUES
-	 (83.0);
+with 
+	avg_order_price as (select p.pizza_id, pt.category, od.order_id,  p.price*od.quantity as order_price  from pizzas p 
+	join order_details od on p.pizza_id = od.pizza_id
+	join orders o on o.order_id = od.order_id 
+	join pizza_types pt on pt.pizza_type_id = p.pizza_type_id
+	group by 1,2,3,4)
+select round(max(order_price),2) as highest_price_of_one_order from avg_order_price
