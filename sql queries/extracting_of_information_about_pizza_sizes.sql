21f76bd8f0846cbafd6fba0f253d0124b420ee21
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-04-04 09:35:40 +0200

    Update extracting_of_information_about_pizza_sizes.sql

--- sql queries/extracting_of_information_about_pizza_sizes.sql
+++ sql queries/extracting_of_information_about_pizza_sizes.sql
@@ -1,6 +1,2 @@
-INSERT INTO order_details (size_data) VALUES
-	 ('m'),
-	 ('l'),
-	 ('s'),
-	 ('xl'),
-	 ('xxl');
+#  BY this query i extract information about the size of the pizza, followed by the word after the last occurrence of the character '_'
+SELECT distinct REVERSE(SUBSTRING_INDEX(REVERSE(pizza_id), '_', 1)) AS size_data FROM order_details od
